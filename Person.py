e4b7881c1b98d537b545528f0a3666a62549e9a4
dagawarudo <189754603+dagawarudo@users.noreply.github.com>
2025-10-11 14:35:06 +0530

    Updated Ride and Person class

--- Person.py
+++ Person.py
@@ -5,7 +5,7 @@ class Person():
         self.xpos = xpos
         self.ypos = ypos
         self.color = color
-        self.collission = False 
+        self.destination = False 
 
     def step_change(self, ride_x, ride_y, ride_list = [], step_size = 1):
         #Source/Inspiration for unit vectors calculations : https://www.wikihow.com/Normalize-a-Vector
@@ -15,15 +15,30 @@ class Person():
 
         if distance == 0:
             # If the person reaches the ride 
-            return
+            self.reached_destination()
         #To get the unit vector 
         x = change_x/distance
         y = change_y/distance
 
         new_x = self.xpos + x * step_size
-        new_y = self.ypos + x * step_size
+        new_y = self.ypos + y * step_size
 
         for ride in ride_list:
+            if ride.is_collides():
+                y *= -1
+                new_y = self.ypos + y * step_size
+        self.xpos = new_x
+        self.ypos = new_y
+    def reached_destination(self):
+        self.destination = True 
+    def get_destination(self):
+        return self.destination
+    def left_destination(self):
+        self.destination = False
+        
+
+
+
 
 
 
--- Ride.py
+++ Ride.py
@@ -4,3 +4,8 @@ class Ride():
         self.ypos = ypos
         self.width = width
         self.height = height
+
+    def is_collides(self, x,y):
+        # Compares the coordinates of an object and rides and checks if the object would be inside the ride 
+        return (self.xpos <= x <= self.xpos + self.width) and (self.ypos <= y <= self.ypos + self.height)
+
